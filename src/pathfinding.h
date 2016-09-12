516dce2b803cb5918c3263dc3ce3dd518740c627
Nathanaël Spriet <nathanael.spriet@gmail.com>
2016-09-12 12:08:01 +0200

    Adds a 'Coordinates' type
    Just a mere coordinates type with x and y, it could be very usefull

--- src/pathfinding.h
+++ src/pathfinding.h
@@ -3,6 +3,14 @@
 
 #ifndef PATHFINDING_H
 
+typedef struct Coordinates Coordinates;
+struct Coordinates
+{
+    int x;
+    int y;
+};
+
+
 void generateGraph(int numRow, int numCol, char graph[numRow][numCol]);
 void launchPathResolution(void);
 void displayGraph(int numRow, int numCol, char graph[numRow][numCol]);
